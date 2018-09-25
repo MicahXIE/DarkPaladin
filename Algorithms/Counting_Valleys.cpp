81617945c1740c1efd7b6e7f9f143b374a66cba3
Micah007PKG <bin.xie.sg@gmail.com>
2018-09-26 01:14:25 +0800

    update

--- Algorithms/Counting_Valleys.cpp
+++ Algorithms/Counting_Valleys.cpp
@@ -1,3 +1,22 @@
+/*
+Sample Input
+
+8
+UDDDUDUU
+Sample Output
+
+1
+Explanation
+
+If we represent _ as sea level, a step up as /, and a step down as \, Gary's hike can be drawn as:
+
+_/\      _
+   \    /
+    \/\/
+He enters and leaves one valley.
+*/
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -5,6 +24,29 @@ using namespace std;
 // Complete the countingValleys function below.
 int countingValleys(int n, string s) {
 
+
+    int level = 0;
+    int start = 0;
+    int count = 0;
+    
+    for(int i=0; i<n; i++){
+        if(s[i] == 'D'){
+            level = level - 1;
+            if(level < 0 && start == 0){
+                start = 1;   
+            }
+        }
+        
+        if(s[i] == 'U'){
+            level = level + 1;
+            if(level == 0 && start == 1){
+                count += 1;
+                start = 0;
+            }
+        }
+    }
+    
+    return count;
 
 }
 
