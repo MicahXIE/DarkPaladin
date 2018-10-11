294fb169f179a360ee1e7939dec918a643d91492
Micah007PKG <bin.xie.sg@gmail.com>
2018-10-11 22:38:09 +0800

    update

---
+++ Algorithms/Tree/Height_Of_A_Binary_Tree.cpp
@@ -0,0 +1,93 @@
+/*
+
+Input Format
+
+The first line contains an integer n, the number of nodes in the tree. 
+Next line contains n space separated integer where ith integer denotes node[i].data.
+
+Output Format
+
+Your function should return a single integer denoting the height of the binary tree.
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        if(root){
+            return 1+max(height(root->left), height(root->right));
+        } else {
+            return -1;
+        }
+    }
+
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    std::cout << height;
+
+    return 0;
+}
+
+
+
+
