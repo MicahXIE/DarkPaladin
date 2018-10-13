c518a6a922a1d988c3884ee8178d62560bd59f55
Micah007PKG <bin.xie.sg@gmail.com>
2018-10-14 01:09:05 +0800

    commit

---
+++ Algorithms/Tree/Is_This_A_Binary_Search_Tree.cpp
@@ -0,0 +1,36 @@
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
+
+The Node struct is defined as follows:
+	struct Node {
+		int data;
+		Node* left;
+		Node* right;
+	}
+*/
+
+	bool checkBST(Node* root) {
+        vector<int> vc;
+        inOrderTraverse(root, &vc);
+        
+        for(int i=1; i<vc.size(); i++){
+            //cout << vc[i] << endl;
+            if(vc[i] > vc[i-1]){
+                continue;
+            }else{
+                return false;
+            }
+        }
+		
+        return true;
+	}
+
+    void inOrderTraverse(Node* root, vector<int> *v){
+        if(root == NULL) return;
+        
+        inOrderTraverse(root->left, v);
+        (*v).push_back(root->data);
+        inOrderTraverse(root->right, v);
+        
+    }
+
+
