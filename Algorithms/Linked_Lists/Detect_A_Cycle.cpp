ae1b31f59b39e143d987574e7b96937f1e9b7850
MicahXIE <bin.xie.sg@gmail.com>
2018-10-31 23:03:51 +0800

    commit

---
+++ Algorithms/Linked_Lists/Detect_A_Cycle.cpp
@@ -0,0 +1,29 @@
+/*
+Hackerrank Linked Lists: Detect a Cycle
+
+idea:
+快慢指针的方法，就是让两个指针同时指向链表。在向后遍历的时候，一个指针每次走两步，称为快指针；
+一个指针每次走一步，称为慢指针。如果快慢指针相遇，则说明链表有环，否则无环。
+
+*/
+
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    if(head == NULL || head->next == NULL){
+        return false;
+    }
+    
+    Node* slow = head;
+    Node* fast = head->next;
+    
+    while(slow != fast){
+        if(fast == NULL || fast->next == NULL) return false;
+        
+        slow = slow->next;
+        fast = fast->next->next;
+        
+    }
+    
+    return true;
+}
