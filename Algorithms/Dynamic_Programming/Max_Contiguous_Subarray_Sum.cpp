0808d0935dbd43d481934fff7d0ec20ba9eff1e3
MicahXIE <bin.xie.sg@gmail.com>
2018-11-17 23:52:25 +0800

    commit

---
+++ Algorithms/Dynamic_Programming/Max_Contiguous_Subarray_Sum.cpp
@@ -0,0 +1,13 @@
+/*
+Maximum Contiguous Subarray Problem (Kadane's algorithm)
+
+Given an array of integers, find contiguous subarray within it which has the 
+largest sum
+
+For example,
+Input: {-2, 1, -3, 4, -1, 2, 1, -5, 4}
+
+Output: subarray {4, -1, 2, 1} with the largest sum 6
+
+https://www.techiedelight.com/maximum-subarray-problem-kadanes-algorithm/
+*/
