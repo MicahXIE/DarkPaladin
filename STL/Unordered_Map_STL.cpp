7b5a96da5d60841a3712c271a576553ea39ac181
MicahXIE <bin.xie.sg@gmail.com>
2019-06-02 22:39:36 +0800

    map stl

---
+++ STL/Unordered_Map_STL.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+int main(int argc, char **argv) {
+    std::unordered_map<int, std::string> map;
+    map.insert(std::make_pair(1, "Scala"));
+    map.insert(std::make_pair(2, "Haskell"));
+    map.insert(std::make_pair(3, "C++"));
+    map.insert(std::make_pair(6, "Java"));
+    map.insert(std::make_pair(14, "Erlang"));
+    std::unordered_map<int, std::string>::iterator it;
+    if ((it = map.find(6)) != map.end()) {
+        std::cout << it->second << std::endl;
+    }
+    return 0;
+}
